50c84707ac65622edd7c3421eedd7852eaf93721
devzelly <149127820+devzelly@users.noreply.github.com>
2023-11-30 18:12:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 58.72 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,18 @@
-// 스택 Stack(LIFO) : push, pop
-// arr에서 하나씩 뽑아서 stack에 push하고 
-// 마지막에 넣은 것이 혹시 중복된 것이면 다시 꺼내기(pop)
+// 스택/큐
+// - 스택 Stack(LIFO) : array.push, array.pop
+// - 큐 Queue(FIFO) : array.push, array.shift
+// 풀이 
+// - arr에서 하나씩 뽑아서 stack에 push하고 
+// - 마지막에 넣은 것이 혹시 중복된 것이면 다시 꺼내기(pop)
+
+const arr = [1, 1, 3, 3, 0, 1, 1];
 function solution(arr) {
   const stack = [];
   for (const num of arr) {
     stack.push(num);
-    if (stack[stack.length-2] === stack[stack.length-1]) {
+    if ( stack.length >= 2 && stack[stack.length-2] === stack[stack.length-1]) {
       stack.pop();
     }
   }
   return stack;
 }
-const arr = [1, 1, 3, 3, 0, 1, 1];
