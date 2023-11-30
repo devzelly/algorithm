7f7dc4057831d6c19b9b3e9bf9e1a2f093ffbb0a
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-01 07:35:39 +0900

    [level 2] Title: 올바른 괄호, Time: 4.71 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -2,10 +2,10 @@
 // - 스택 Stack(LIFO) : array.push, array.pop
 // - 큐 Queue(FIFO) : array.push, array.shift
 // 풀이 
-// - parenthesis 
+// - 괄호 parenthesis 
 // - 시작은 ( , 끝은 ) 
 // - (은 -1 )은 +1로 계산해서 최종 0이 되는지 검증
-// - 양수로 전환되면 false
+// - 양수로 전환되면 false 이다
 
 function solution(s) {     
     let result = 0; 
@@ -17,10 +17,9 @@ function solution(s) {
         if(char === '(') {
             result-- ;
         } else {
-            result++ ;  
-            if (result > 0) {
+            result++ ; 
+            if(result > 0) 
                 return false;
-            }
         }
     }      
     return result === 0 ;  
