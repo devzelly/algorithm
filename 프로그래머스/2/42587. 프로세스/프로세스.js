2b3e366987c87bb1e4051efae2956be7babed451
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-03 19:06:28 +0900

    [level 2] Title: 프로세스, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,25 +1,33 @@
+const priorities = [1, 1, 9, 1, 1, 1];
+
 function solution(priorities, location) {
-  // queue 배열 : idx, 값
-  // { 0,  2 }
-  // { 1,  1 }
-  // { 2,  3 }
-  // { 3,  2 }
   const queue = priorities.map((val, idx) => ({ idx, val }));
-  let count = 0;
+  //    idx , val      order 순서
+  // { idx:1, val:1 }
+  // { idx:0, val:1 }
+  // { idx:2, val:9 }    2    1
+  // { idx:3, val:1 }    3    2
+  // { idx:4, val:1 }    4    3
+  // { idx:5, val:1 }    5    4
+  //                     1    5
+  //                     2    6
+
+  let order = [];
+  let orderofLoc = 0;
 
   while (queue.length > 0) {
-    // current : 0번부터 뽑기
     const current = queue.shift();
-
-    // 더 큰수가 있으면 뒤로 push
+    // 더 큰수가 있으면 뒤로 push로 붙임
     if (queue.some((item) => item.val > current.val)) {
       queue.push(current);
     } else {
-      // 더 큰수가 없으면
-      count++;
-      if (current.idx === location) {
-        return count;
-      }
+      // cur가 가장 큰수라면
+      order.push(current.idx);
+      // 9면            2
+      // 실행순서(idx) : 2, 3, 4, 5, 1, 2  → location 입력값
     }
   }
+
+  orderofLoc = order.indexOf(location) + 1;
+  return orderofLoc;
 }
