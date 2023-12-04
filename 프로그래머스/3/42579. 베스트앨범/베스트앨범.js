e32926e77b02fa0f1ad5501de5bcf75622ad5584
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-04 13:43:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -9,29 +9,33 @@ function solution(genres, plays) {
 	let genresHash = {}; // 장르별 누적 재생 횟수를 저장할 객체
 	let songs = []; // 노래 정보를 저장할 배열
 
+	// 1. 장르별 총조회수 객체
+	// 2. 노래 정보 객체 배열
 	for (let i = 0; i < genres.length; i++) {
 		const genre = genres[i];
-		// 장르별 합계
+		// 객체 : 장르별 합계
 		genresHash[genre] = (genresHash[genre] || 0) + plays[i];
-		// 노래정보      idx       장르         재생수
+		// 배열(객체) :   idx       장르         재생수
 		songs.push({ index: i, genre: genre, play: plays[i] });
 	}
 
-	// 장르별 총재생수 내림차순
+	// 장르별 총조회수 내림차순
 	let descGenres = Object.keys(genresHash).sort(
 		(a, b) => genresHash[b] - genresHash[a]
 	);
 
-	// 장르별로 높은 횟수 2개 선택
 	let result = [];
 
+	// 장르별로 높은 횟수 2개 선택
 	for (let genre of descGenres) {
-		// 객체 배열 필터링
+		// 장르 우선순위 선택
+		// 배열(객체) 필터링:장르
 		let genreSongs = songs.filter((song) => song.genre === genre);
-		// 조회수 내림차순
+		// 장르송 내림차순
 		genreSongs.sort((a, b) => b.play - a.play);
 
-		// (장르 우선순위로) 2순위까지 push
+		// 2순위까지 idx → push
+		// Math.min : 2곡이 안되는 경우
 		for (let i = 0; i < Math.min(genreSongs.length, 2); i++) {
 			result.push(genreSongs[i].index);
 		}
