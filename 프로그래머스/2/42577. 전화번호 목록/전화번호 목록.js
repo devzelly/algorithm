0054d77447a79079a5cb55cbdb38c9965c0ccdb3
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-04 11:46:01 +0900

    [level 2] Title: 전화번호 목록, Time: 470.82 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -2,37 +2,37 @@
 // ["119", "97674223", "1195524421"]	false
 // ["123","456","789"]	true
 // ["12","123","1235","567","88"]	false
-
 phone_book = ["97674223", "1195524421", "119"];
-
 function solution(phone_book) {
 	let result = true;
+	// 오름차순 길이 정렬
 	let pb = phone_book.sort((a, b) => {
 		return a.length - b.length;
 	});
 
 	// 객체 선언
-	let phoneBookMap = {};
+	let pbHash = {};
 	// {
 	//    97674223 : true,
+	//    1195524421 : true,
 	//    119 : true,
 	// }
 
 	for (let i = 0; i < pb.length; i++) {
 		for (let j = 1; j < pb[i].length; j++) {
-			// 최초에 break;
-			if (phoneBookMap.length === 0) {
+			// 최초에 접두어 없을때 break;
+			if (pbHash.length === 0) {
 				break;
 			}
-			// 각 번호의 글자수대로 뽑아서 해쉬 key 비교
+			// 각 번호의 글자수대로 뽑아서 접두어(해쉬 key) 체크
+			// 자신이 피접두어라면, false 끝
 			let prefix = pb[i].substring(0, j);
-			// 자신이 피접두어인지 확인 → 맞으면 false 끝
-			if (phoneBookMap[prefix]) {
+			if (pbHash[prefix]) {
 				return false;
 			}
 		}
 		// (자신이 피접두어가 아니면) 접두어로 등록
-		phoneBookMap[pb[i]] = true;
+		pbHash[pb[i]] = true;
 	}
 	return result;
 }
