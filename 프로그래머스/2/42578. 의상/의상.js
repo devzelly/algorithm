69426fd62d8d5fe8cbeaf7d752134078e170ae67
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-04 12:40:00 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,34 @@
+// 해시
+
+clothes = [
+	["yellow_hat", "headgear"],
+	["blue_sunglasses", "eyewear"],
+	["green_turban", "headgear"],
+];
+
+function solution(clothes) {
+	clothesHash = {};
+	let sum = 0;
+	let mul = 1;
+
+	// 객체 : 해시맵 작성
+	for (let i = 0; i < clothes.length; i++) {
+		let clothesType = clothes[i][1];
+		clothesHash[clothesType] = clothesHash[clothesType] || 0;
+		clothesHash[clothesType]++;
+	}
+	// clothesHash =
+	// [
+	//     [ headgear, 2 ],
+	//     [ eyewear, 1 ]
+	// ]
+
+	// 경우의 수
+	for (let type in clothesHash) {
+		mul *= clothesHash[type] + 1; // 3 * 2 = 6
+	}
+
+	return mul - 1; // 미착용인 경우 제외
+}
+
+console.log(solution(clothes));
