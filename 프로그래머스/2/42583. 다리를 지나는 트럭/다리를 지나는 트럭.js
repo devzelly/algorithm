4bd3b5cef1e6075b73c43462ce70d7de2f9f3cf9
devzelly <149127820+devzelly@users.noreply.github.com>
2023-12-04 09:23:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,30 +1,40 @@
+// 예시 테스트
+// solution(2, 10, [7, 4, 5, 6])  // 8
+// solution(100, 100, [10])  // 101
+// solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])  // 110
+
+//               다리블럭길이=초  총무게    트럭배열
+//                      2,          10,   [ 4, 5, 6 ]
+
+// 1초지남 : 1.이동 → 2.제거(무게계산) → 3.추가(1블럭에)
+
 function solution(bridge_length, weight, truck_weights) {
-    let answer = 1;
-    let onBridge = [];
-    let totalWeight = 0;
+	let time = 0;
+	let truch_Bridge = [];
+	let sumWeight = 0;
 
-    while (onBridge.length > 0 || truck_weights.length > 0) {
-        // 이전에 answer를 1로 초기화했으므로 루프 시작 시에 answer를 증가시키지 않음
+	while (truch_Bridge.length > 0 || truck_weights.length > 0) {
+		time++;
 
-        // 트럭이 다리에 올라갈 수 있는지 확인
-        if (truck_weights.length > 0 && totalWeight + truck_weights[0] <= weight) {
-            const truck = truck_weights.shift();
-            onBridge.push([truck, 0]);  // 다리에 트럭 추가 (두 번째 값은 시간을 나타냄)
-            totalWeight += truck;
-        }
+		// 다리 위의 트럭을 한 칸씩 이동
+		for (let i = 0; i < truch_Bridge.length; i++) {
+			truch_Bridge[i][1]++;
+		}
 
-        // 다리 위의 트럭을 한 칸씩 이동
-        for (let i = 0; i < onBridge.length; i++) {
-            onBridge[i][1]++;
-        }
+		// 도착 트럭 제거 + 무게계산
+		if (truch_Bridge.length > 0 && truch_Bridge[0][1] > bridge_length) {
+			// if (truch_Bridge[0][1] > bridge_length) {
+			let remove = truch_Bridge.shift();
+			sumWeight -= remove[0];
+		}
 
-        // 다리를 다 건넌 트럭 제거
-        if (onBridge.length > 0 && onBridge[0][1] === bridge_length) {
-            totalWeight -= onBridge.shift()[0];
-        }
-
-        answer++;  // 루프가 한 번 실행될 때마다 answer를 1 증가시킴
-    }
-
-    return answer;
+		// 트럭을 블럭1에 추가 or 스킵
+		// if (truck_weights.length > 0 && sumWeight + truck_weights[0] <= weight) {
+		if (truck_weights[0] && sumWeight + truck_weights[0] <= weight) {
+			const truck = truck_weights.shift();
+			truch_Bridge.push([truck, 1]); // 다리에 트럭 추가. 1블럭에
+			sumWeight += truck;
+		}
+	}
+	return time;
 }
